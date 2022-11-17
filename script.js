98e727f3d95069830b7f256a3ba5c7dcaf23df99
Aromat06 <lennywuersten6@gmail.com>
2022-11-17 12:05:56 +0100

    correction

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function handleKeyDown(ev)  {
     // console.log(ev)
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
-    player.setAttribute("cx", 100 * Math.random())
+    player.setAttribute("cy", 100 * Math.random())
 }
 
 
